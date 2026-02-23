c6dac400a656093103036cd6c34b60c204ae2f64
ricardo220889 <ricardo220889@gmail.com>
2026-02-23 16:42:37 -0600

    version 1.2

---
+++ app/src/main/java/com/example/cadenacustodiapp/ui/EmbalajeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.cadenacustodiapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cadenacustodiapp.R
+
+class EmbalajeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_embalaje, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/cadenacustodiapp/ui/TrasladoFragment.kt
@@ -0,0 +1,24 @@
+package com.example.cadenacustodiapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.example.cadenacustodiapp.R
+
+
+class TrasladoFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_traslado, container, false)
+    }
+
+
+}
